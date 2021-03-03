81aadf37be6888f6ac479163869f13bc790b5eca
Enrique <kike.csw@gmail.com>
2021-03-03 18:06:50 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+
+public class Controller {
+
+
+    @FXML
+    AnchorPane Luxray;
+
+
+    public void fondo(MouseEvent mouseEvent) {
+    }
+}
